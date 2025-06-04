311cd34f0c36f099206091b5819c42711df6732f
russellcolestock <92215495+russellcolestock@users.noreply.github.com>
2025-06-03 20:39:36 -0500

    Initial commit

---
+++ index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+
+function App() {
+    return (
+      <BrowserRouter>
+          <nav>
+              <h1>#VANLIFE</h1>
+              <div className='nav-links-container'>
+                  <Link to="/" className='nav-link'>Home</Link>
+                  <Link to="/about" className='nav-link'>About</Link>            
+              </div>
+          </nav>
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+          </Routes>
+          <footer>Ⓒ 2022 #VANLIFE</footer>
+      </BrowserRouter>
+    )
+  }
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
---
+++ pages/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default   function About() {
+      return (
+        <div className='about-main'>
+            <img src='./images/van-night.png' />
+            <div className='about-main-info'>
+                <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+                <p>Our mission is to enliven your road trip with the perfect travel 
+                    van rental. Our vans are recertified before each trip to ensure your 
+                    travel plans can go off without a hitch.
+                    <br/>(Hitch costs extra 😉)
+                    <br/>
+                    <br/>Our team is full of vanlife enthusiasts who know firsthand the magic 
+                    of touring the world on 4 wheels.
+                </p>
+                <div>
+                    <h2>Your destination is waiting.<br/>Your van is ready.</h2> 
+                    <button>Explore our vans</button>
+                </div>
+
+            </div>
+        </div>
+      )
+  }
