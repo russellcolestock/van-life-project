3f22bf641943b109d4b1c1ec012d0406aa012415
russellcolestock <92215495+russellcolestock@users.noreply.github.com>
2025-06-05 15:07:26 -0500

    Updates to Van.jsx

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 export default function Vans() {
+    const [vanData, setVanData] = useState([])
+
+    useEffect( () => {
+        fetch('/api/vans')
+            .then(res => res.json())
+            .then(data => {
+                setVanData(data.vans)
+            })
+    }, [] )
+
     return (
-        <h1>Vans page goes here 🚐</h1>
+        <div>
+            <h1>Vans page goes here 🚐</h1>
+            {vanData.map(van => (
+                <div key={van.id}>
+                    <img src={van.imageURL} />
+                    <div>
+                        <h2>{van.name}</h2>
+                        <p>{van.price}</p>
+                    </div>
+                    <button>{van.type}</button>
+                </div>
+            ))}
+        </div>
     )
 }
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ createServer({
             name: "Modest Explorer", 
             price: 60, 
             description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!", 
-            imageUrl: "./images/modest-explorer.png", 
+            imageUrl: "/images/modest-explorer.png", 
             type: "simple" })
         server.create("van", { 
             id: "2", 
