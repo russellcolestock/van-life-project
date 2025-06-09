06b40e7e1d2040d45b5314516979857c3ca7dca1
russellcolestock <92215495+russellcolestock@users.noreply.github.com>
2025-06-09 06:46:47 -0500

    Created components folder
    Created components folder and added Header and Layout files.  Refactored code to make the header a nested route.

---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+export default function Header() {
+    return (
+         <header>
+          <nav>
+              <Link className="site-logo" to="/">#VanLife</Link>
+              <div className='nav-links-container'>
+                  <Link to="/about" className='nav-link'>About</Link>  
+                  <Link to="/vans" className='nav-link'>Vans</Link>          
+              </div>
+          </nav>
+        </header>
+    )
+}
--- index.jsx
+++ index.jsx
@@ -5,29 +5,21 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Vans from './pages/Vans';
 import VanDetail from './pages/VanDetail';
+import Layout from './components/Layout'
 
 import "./server"
 
 function App() {
     return (
       <BrowserRouter>
-        <header>
-          <nav>
-              <Link className="site-logo" to="/">#VanLife</Link>
-              <div className='nav-links-container'>
-                  <Link to="/about" className='nav-link'>About</Link>  
-                  <Link to="/vans" className='nav-link'>Vans</Link>          
-              </div>
-          </nav>
-
-        </header>
           <Routes>
+            <Route element={<Layout />}>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
               <Route path='/vans' element={<Vans />} />
               <Route path='/vans/:id' element={<VanDetail />} />
-
-          </Routes>
+            </Route> 
+        </Routes> 
           <footer>Ⓒ 2022 #VANLIFE</footer>
       </BrowserRouter>
     )
