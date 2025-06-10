2ed24430e2c0efb926ef48c99c353e2f3e374e21
russellcolestock <92215495+russellcolestock@users.noreply.github.com>
2025-06-10 12:45:08 -0500

    Update Dashboard.jsx

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,11 +1,8 @@
 import React from "react"
-import { Outlet } from "react-router-dom"
 
 export default function Dashboard() {
     return (
-        <>
-            <h1>Dashboard here</h1>
-            <Outlet />
-        </>
+        
+        <h1>Dashboard here</h1>
     )
 }
