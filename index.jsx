08ddb6c240841d3e106857e1acdb20c3a38ee730
russellcolestock <92215495+russellcolestock@users.noreply.github.com>
2025-06-10 20:58:05 -0500

    UPdates to HostJayout, index.jsx, and index.css

--- index.jsx
+++ index.jsx
@@ -11,6 +11,7 @@ import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
 
 import "./server"
+import HostLayout from './components/HostLayout';
 
 function App() {
     return (
@@ -21,8 +22,8 @@ function App() {
               <Route path='/about' element={<About />} />
               <Route path='/vans' element={<Vans />} />
               <Route path='/vans/:id' element={<VanDetail />} />
-              
-              <Route path='/host' element={<Dashboard />} >
+
+              <Route path='/host' element={<HostLayout />} >
                 <Route path='/host/income' element={<Income />} />
                 <Route path='/host/reviews' element={<Reviews />} />
               </Route>
