d0ce68950ac1353536e99beb112fea64c734e8fc
russellcolestock <92215495+russellcolestock@users.noreply.github.com>
2025-06-12 10:30:00 -0500

    Update HostLayout.jsx
    changed Link to NavLink and added some conditional styling

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function HostLayout() {
+
+    const activeStyle = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
     return (
         <>
             <nav className="host-layout-nav">
-                <Link to="/host">Dashboard</Link>
-                <Link to="/host/income">Income</Link>
-                <Link to="/host/reviews">Reviews</Link>
+                <NavLink 
+                    to="/host"
+                    end /*The end prop tells React Router to end the matching here.
+                        So if a more nested route matches, it won't also match this
+                        link on this route. */ 
+                    style={ ({isActive}) => isActive ? activeStyle : null }
+                >
+                    Dashboard
+                </NavLink>
+                <NavLink 
+                    to="/host/income"
+                    style={ ({isActive}) => isActive ? activeStyle : null }
+                >
+                    Income
+                </NavLink>
+                <NavLink 
+                    to="/host/reviews"
+                    style={ ({isActive}) => isActive ? activeStyle : null }
+                >
+                    Reviews
+                </NavLink>
             </nav>
             <Outlet />
         </>
