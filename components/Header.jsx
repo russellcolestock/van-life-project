a31383852ba410d408c310a31a06460d0b686405
russellcolestock <92215495+russellcolestock@users.noreply.github.com>
2025-06-12 10:03:38 -0500

    Update Header.jsx
    changed Link to NavLink and added style for better UI while navigating links in the navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
+    const activeStyle = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+
+    }
+
     return (
          <header>
           <nav>
               <Link className="site-logo" to="/">#VanLife</Link>
               <div className='nav-links-container'>
-                <Link to="/host" className='nav-link'>Host</Link>
-                <Link to="/about" className='nav-link'>About</Link>  
-                <Link to="/vans" className='nav-link'>Vans</Link>          
+                <NavLink 
+                    to="/host" className='nav-link'
+                    style={ ({isActive}) => isActive ? activeStyle : null }          
+                >
+                  Host
+                </NavLink>
+                <NavLink 
+                    to="/about" className='nav-link'
+                    style={ ({isActive}) => isActive ? activeStyle : null }
+                >
+                  About
+                </NavLink>  
+                <NavLink 
+                    to="/vans" className='nav-link'
+                    style={ ({isActive}) => isActive ? activeStyle : null }
+                >
+                  Vans
+                </NavLink>          
               </div>
           </nav>
         </header>
