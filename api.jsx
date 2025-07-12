e7e6fe916ca2b550b7e2ba01370198bb773c91e3
russellcolestock <92215495+russellcolestock@users.noreply.github.com>
2025-07-12 10:32:50 -0500

    Update api.jsx
    Updated the getHostVans function to use Firebase rather than Mirage.js

--- api.jsx
+++ api.jsx
@@ -4,7 +4,9 @@ import {
     collection, 
     doc, 
     getDocs, 
-    getDoc 
+    getDoc,
+    query,
+    where
 } from "firebase/firestore/lite"
 
 const firebaseConfig = {
@@ -35,42 +37,36 @@ export async function getVans() {
 export async function getVan(id) {
     const docRef = doc(db, "vans", id )
     const snapshot = await getDoc(docRef)
-    
+
     return {
         ...snapshot.data(),
         id: snapshot.id
     }
 }
 
-// export async function getVans(id) {
-//     const url = id ? `/api/vans/${id}` : "/api/vans"
-//     console.log(url)
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
 
+// export async function getHostVans(id) {
+//     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
 //     const res = await fetch(url)
 //     if (!res.ok) {
 //         throw {
-//             message: "Failed to fetch vans", 
+//             message: "Failed to fetch vans",
 //             statusText: res.statusText,
 //             status: res.status
 //         }
 //     }
 //     const data = await res.json()
 //     return data.vans
-// } 
-
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
-}
+// }
 
 
 export async function loginUser(creds) {
